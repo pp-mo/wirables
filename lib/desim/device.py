c1e5878498b6ae8085498ef8e705afd7917f4007
Patrick Peglar <patrick.peglar@metoffice.gov.uk>
2026-01-15 14:53:59 +0000

    Reorder Device methods for better clarity.

--- lib/desim/device.py
+++ lib/desim/device.py
@@ -38,8 +38,157 @@ class Device:
     def reset(self):
         self.state = "idle"
 
+    # Device inputs, actions and outputs.
+    # All are subclass-specific.
+    # inputs + actions are methods, wrapped with common behaviours
+    # outputs are just Signals.
+
+    @staticmethod
+    def _wrap_functype(inner_func, label: str) -> InputAndActionClassCallsType:
+        """
+        A basic decorator for wrapping input+action functions.
+
+        Provides common behaviours, including "hooks" and labelling.
+        Expects signatures like input(time, value) and action(time[, value[, context]]).
+        Converts the wrapped function into an EventClient instance.
+        """
+        name = inner_func.__name__
+
+        @wraps(inner_func)
+        def wrapper_call(
+            self,
+            time: EventTime,
+            value: EventValue | int | float | str | None = None,
+            context=None,
+        ):
+            time = EventTime(time)
+            match value:
+                case None:
+                    value = None
+                case EventValue() | int() | float() | str():
+                    value = EventValue(value)
+                case _:
+                    msg = f"'value' {value!r} has unsupported type."
+                    raise TypeError(msg)
+            args: tuple = (time,)
+            if context is not None:
+                args += (value, context)
+            elif value is not None:
+                args += (value,)
+            self.call_hooks(
+                name=name,
+                hookset=self._prehooks,
+                time=time,
+                value=value,
+                context=context,
+            )
+            inner_results = inner_func(self, *args)  # NB explicit self is needed here
+            self.call_hooks(
+                name=name,
+                hookset=self._posthooks,
+                time=time,
+                value=value,
+                context=context,
+            )
+            if inner_results:
+                self._further_acts += inner_results
+            # now **remove** any created events acts from the static list + return them.
+            results = self._further_acts
+            self._further_acts = []
+            return results
+
+        setattr(wrapper_call, label, True)
+        # TODO: typing this seems tricky.  Possibly needs more control over @wraps() ?
+        return wrapper_call
+
+    @staticmethod
+    def action(inner_func) -> InputAndActionClassCallsType:
+        """Decorator to make an action function."""
+        return Device._wrap_functype(inner_func, "_label_action")
+
+    @staticmethod
+    def input(inner_func) -> InputAndActionClassCallsType:
+        """Decorator to make an input function."""
+        return Device._wrap_functype(inner_func, "_label_input")
+
+    # Properties with dicts of all the device inputs + actions.
+    # NOTE: these are calculated dynamically, instead of in the "@action/input"
+    # decorators, because we want *instance* methods (so, not available at class
+    # definition time).
+    # TODO: they could be cached, though ?
+
+    def _map_labelled_funcs(
+        self, label: str
+    ) -> dict[str, InputAndActionInstanceCallsType]:
+        """Return a dict of the device's inputs with a particular label.
+
+        This enables us to identify inputs + actions.
+        Doing it this way to avoid using a metaclass.
+        """
+        cls = self.__class__
+        all_attrs = {name: getattr(cls, name) for name in dir(cls)}
+        wanted_calls = {
+            name: getattr(self, name)  # get the INSTANCE method
+            for name, call in all_attrs.items()
+            if callable(call) and hasattr(call, label)
+        }
+        return wanted_calls
+
+    @property
+    def actions(self) -> dict[str, InputAndActionInstanceCallsType]:
+        """Return a dict of the device's actions."""
+        return self._map_labelled_funcs("_label_action")
+
+    @property
+    def inputs(self) -> dict[str, InputAndActionInstanceCallsType]:
+        """Return a dict of the device's inputs."""
+        return self._map_labelled_funcs("_label_input")
+
+    def add_output(self, name: str, start_value: EventValue = SIG_START_DEFAULT):
+        """Create an output signal.
+
+        Outputs are normally created in init, and assigned to the instance for ease of
+        use.  E.G. "self.out1 = self.add_output('out1')".
+
+        This routine also installs them in the "self.outputs" dict, and as instance
+        properties.
+        """
+        output_signal = Signal(name=name, start_value=start_value)
+        self.outputs[name] = output_signal
+        setattr(self, name, output_signal)
+        return output_signal
+
+    # @action methods are called by an Event to implement a delayed operation.
+    # These events are themselves usually created by a Device.act() call in an @input or
+    # other @action method.
+
+    def act(
+        self,
+        time: EventTime | float | int,
+        action_name: str,
+        value: EventValue | float | int | str | None = None,
+        context=None,
+    ):
+        """
+        Schedule a subsequent call (at a later simulation time) to one of this device's
+        @action methods.
+
+        This creates a new Event and adds it to self._further_acts, from where it is
+        eventually returned to the caller of the device function (input or action), as
+        standard behaviour of action+input methods (from the common code wrapper).
+        """
+        time = EventTime(time)
+        if value is not None:
+            value = EventValue(value)
+        action = self.actions.get(action_name)
+        assert callable(action) and hasattr(action, "_label_action")
+        event = Event(time, action, value, context)
+        self._further_acts.append(event)
+
     # Device hooks + tracing.
-    # TODO: tracing should *not* be embedded in Device, but implemented via hooks.
+    # TODO: tracing should *not* be embedded in the Device code,
+    #  but implemented via hooks.
+    #  The storage + handling will still need an instance variable, though.
 
     def hook(
         self, name: str, call: EventClient, context=None, call_after=False
@@ -182,140 +331,3 @@ class Device:
                         ]
                         for tracehook in tracehooks:
                             self.unhook(tracehook)
-
-    @staticmethod
-    def _wrap_functype(inner_func, label: str) -> InputAndActionClassCallsType:
-        """
-        A basic decorator for wrapping input+action functions.
-
-        Provides common behaviours, including "hooks" and labelling.
-        Expects signatures like input(time, value) and action(time[, value[, context]]).
-        Converts the wrapped function into an EventClient instance.
-        """
-        name = inner_func.__name__
-
-        @wraps(inner_func)
-        def wrapper_call(
-            self,
-            time: EventTime,
-            value: EventValue | int | float | str | None = None,
-            context=None,
-        ):
-            time = EventTime(time)
-            match value:
-                case None:
-                    value = None
-                case EventValue() | int() | float() | str():
-                    value = EventValue(value)
-                case _:
-                    msg = f"'value' {value!r} has unsupported type."
-                    raise TypeError(msg)
-            args: tuple = (time,)
-            if context is not None:
-                args += (value, context)
-            elif value is not None:
-                args += (value,)
-            self.call_hooks(
-                name=name,
-                hookset=self._prehooks,
-                time=time,
-                value=value,
-                context=context,
-            )
-            inner_results = inner_func(self, *args)  # NB explicit self is needed here
-            self.call_hooks(
-                name=name,
-                hookset=self._posthooks,
-                time=time,
-                value=value,
-                context=context,
-            )
-            if inner_results:
-                self._further_acts += inner_results
-            # now **remove** any created events acts from the static list + return them.
-            results = self._further_acts
-            self._further_acts = []
-            return results
-
-        setattr(wrapper_call, label, True)
-        # TODO: typing this seems tricky.  Possibly needs more control over @wraps() ?
-        return wrapper_call
-
-    @staticmethod
-    def action(inner_func) -> InputAndActionClassCallsType:
-        """Decorator to make an action function."""
-        return Device._wrap_functype(inner_func, "_label_action")
-
-    @staticmethod
-    def input(inner_func) -> InputAndActionClassCallsType:
-        """Decorator to make an input function."""
-        return Device._wrap_functype(inner_func, "_label_input")
-
-    def connect(self, input_name: str, signal: Signal):
-        """Connect a signal to a device input."""
-        signal.connect(self.inputs[input_name])
-
-    def act(
-        self,
-        time: EventTime | float | int,
-        action_name: str,
-        value: EventValue | float | int | str | None = None,
-        context=None,
-    ):
-        """
-        Schedule a call (later) to one of this device's actions.
-        This should *only* be called from another action or input function.
-
-        Usually, this will cause a state transition after a time delay.
-        """
-        time = EventTime(time)
-        if value is not None:
-            value = EventValue(value)
-        action = self.actions.get(action_name)
-        assert callable(action) and hasattr(action, "_label_action")
-        event = Event(time, action, value, context)
-        self._further_acts.append(event)
-
-    def _map_labelled_funcs(
-        self, label: str
-    ) -> dict[str, InputAndActionInstanceCallsType]:
-        """Return a dict of the device's inputs with a particular label.
-
-        This enables us to identify inputs + actions.
-        Doing it this way to avoid using a metaclass.
-        """
-        cls = self.__class__
-        all_attrs = {name: getattr(cls, name) for name in dir(cls)}
-        wanted_calls = {
-            name: getattr(self, name)  # get the INSTANCE method
-            for name, call in all_attrs.items()
-            if callable(call) and hasattr(call, label)
-        }
-        return wanted_calls
-
-    # Properties listing all device inputs + actions.
-    # NOTE: calculated dynamically, instead of in the "@action" decorator, because we
-    # want *instance* methods, which are not available at class definition time.
-    # TODO: they could be cached, though ?
-
-    @property
-    def actions(self) -> dict[str, InputAndActionInstanceCallsType]:
-        """Return a dict of the device's actions."""
-        return self._map_labelled_funcs("_label_action")
-
-    @property
-    def inputs(self) -> dict[str, InputAndActionInstanceCallsType]:
-        """Return a dict of the device's inputs."""
-        return self._map_labelled_funcs("_label_input")
-
-    def add_output(self, name: str, start_value: EventValue = SIG_START_DEFAULT):
-        """Create an output signal.
-
-        Outputs are normally created in init, and assigned to the instance for ease of
-        use.
-        E.G. "self.out1 = self.add_output("out1")".
-        This routine also installs them in the "self.outputs" dict.
-        """
-        output_signal = Signal(name=name, start_value=start_value)
-        self.outputs[name] = output_signal
-        return output_signal
